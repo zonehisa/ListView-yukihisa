0dcce289ca1ec55ee232384870df8824a8e442ea
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-09 13:50:06 +0900

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by 佐藤幸久 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
